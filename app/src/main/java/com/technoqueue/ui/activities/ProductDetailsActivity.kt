6fa824c2ff6eee1d8eedf701677d79c8d094e3bc
Deshun99 <e0560014@u.nus.edu>
2021-06-06 20:43:13 +0800

    Displayed product details on dashboard and from product tab

---
+++ app/src/main/java/com/technoqueue/ui/activities/ProductDetailsActivity.kt
@@ -0,0 +1,89 @@
+package com.technoqueue.ui.activities
+
+import android.os.Bundle
+import android.util.Log
+import com.technoqueue.R
+import com.technoqueue.firestore.FirestoreClass
+import com.technoqueue.models.Product
+import com.technoqueue.utils.Constants
+import com.technoqueue.utils.GlideLoader
+import kotlinx.android.synthetic.main.activity_product_details.*
+
+/**
+ * Product Details Screen.
+ */
+class ProductDetailsActivity : BaseActivity() {
+
+    // A global variable for product id.
+    private var mProductId: String = ""
+
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_product_details)
+
+        if (intent.hasExtra(Constants.EXTRA_PRODUCT_ID)) {
+            mProductId =
+                intent.getStringExtra(Constants.EXTRA_PRODUCT_ID)!!
+            Log.i("Product Id", mProductId)
+        }
+
+        setupActionBar()
+
+        getProductDetails()
+    }
+
+    /**
+     * A function for actionBar Setup.
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(toolbar_product_details_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+        }
+
+        toolbar_product_details_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    /**
+     * A function to call the firestore class function that will get the product details from cloud firestore based on the product id.
+     */
+    private fun getProductDetails() {
+
+        // Show the product dialog
+        showProgressDialog(resources.getString(R.string.please_wait))
+
+        // Call the function of FirestoreClass to get the product details.
+        FirestoreClass().getProductDetails(this@ProductDetailsActivity, mProductId)
+    }
+
+    /**
+     * A function to notify the success result of the product details based on the product id.
+     *
+     * @param product A model class with product details.
+     */
+    fun productDetailsSuccess(product: Product) {
+
+        // Hide Progress dialog.
+        hideProgressDialog()
+
+        // Populate the product details in the UI.
+        GlideLoader(this@ProductDetailsActivity).loadProductPicture(
+            product.image,
+            iv_product_detail_image
+        )
+
+        tv_product_details_title.text = product.title
+        tv_product_details_price.text = "$${product.price}"
+        tv_product_details_description.text = product.description
+        tv_product_details_stock_quantity.text = product.stock_quantity
+    }
+}
---
+++ app/src/main/java/com/technoqueue/ui/adapters/MyProductsListAdapter.kt
@@ -0,0 +1,68 @@
+package com.technoqueue.ui.adapters
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.technoqueue.R
+import com.technoqueue.models.Product
+import com.technoqueue.ui.activities.ProductDetailsActivity
+import com.technoqueue.ui.fragments.ProductsFragment
+import com.technoqueue.utils.Constants
+import com.technoqueue.utils.GlideLoader
+import kotlinx.android.synthetic.main.item_list_layout.view.*
+
+open class MyProductsListAdapter (
+
+    private val context: Context,
+    private var list: ArrayList<Product>,
+    private val fragment: ProductsFragment
+): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MyViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.item_list_layout,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+
+        if (holder is MyViewHolder) {
+
+            GlideLoader(context).loadProductPicture(model.image, holder.itemView.iv_item_image)
+
+            holder.itemView.tv_item_name.text = model.title
+            holder.itemView.tv_item_price.text = "$${model.price}"
+
+            // TODO Step 4: Assigning the click event to the delete button.
+            // START
+            holder.itemView.ib_delete_product.setOnClickListener {
+
+                // TODO Step 8: Now let's call the delete function of the ProductsFragment.
+                // START
+                fragment.deleteProduct(model.product_id)
+                // END
+            }
+
+            holder.itemView.setOnClickListener {
+                val intent = Intent(context, ProductDetailsActivity::class.java)
+                intent.putExtra(Constants.EXTRA_PRODUCT_ID, model.product_id)
+                context.startActivity(intent)
+            }
+            // END
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view)
+}
